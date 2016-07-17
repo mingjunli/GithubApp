91df7ddbb14fd25fd950b2f03c9bcfd735fac930
mingjunli <anly.pear@gmail.com>
2016-07-17 17:39:25 +0800

    wrap logger into AppLog

--- app/src/main/java/com/anly/githubapp/GithubApplication.java
+++ app/src/main/java/com/anly/githubapp/GithubApplication.java
@@ -2,8 +2,18 @@ package com.anly.githubapp;
 
 import android.app.Application;
 
+import com.anly.githubapp.common.util.AppLog;
+
 /**
  * Created by mingjun on 16/7/15.
  */
 public class GithubApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // init logger.
+        AppLog.init();
+    }
 }
---
+++ app/src/main/java/com/anly/githubapp/common/util/AppLog.java
@@ -0,0 +1,57 @@
+package com.anly.githubapp.common.util;
+
+import com.anly.githubapp.BuildConfig;
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by mingjun on 16/7/17.
+ */
+public class AppLog {
+
+    private static final String TAG = "GithubApp";
+
+    /**
+     * initialize the logger.
+     */
+    public static void init() {
+        Logger.init(TAG);
+    }
+
+    /**
+     * log.i
+     * @param msg
+     */
+    public static void i(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.i(msg);
+        }
+    }
+
+    /**
+     * log.d
+     * @param msg
+     */
+    public static void d(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.d(msg);
+        }
+    }
+
+    /**
+     * log.w
+     * @param msg
+     */
+    public static void w(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.w(msg);
+        }
+    }
+
+    /**
+     * log.e
+     * @param msg
+     */
+    public static void e(String msg) {
+        Logger.e(msg);
+    }
+}
