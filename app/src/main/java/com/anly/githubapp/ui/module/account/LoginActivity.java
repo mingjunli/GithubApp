e127ca39bc17b40d84241729909146e70213203e
mingjunli <anly.pear@gmail.com>
2016-07-19 19:29:33 +0800

    add login activity

--- app/src/main/java/com/anly/githubapp/ui/module/account/LoginActivity.java
+++ app/src/main/java/com/anly/githubapp/ui/module/account/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.anly.githubapp.ui.module.account;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.anly.githubapp.R;
 import com.anly.githubapp.ui.base.BaseActivity;
 
 public class LoginActivity extends BaseActivity {
+
+    public static void launch(Context context) {
+        context.startActivity(new Intent(context, LoginActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/anly/githubapp/ui/module/main/MainActivity.java
+++ app/src/main/java/com/anly/githubapp/ui/module/main/MainActivity.java
@@ -3,6 +3,10 @@ package com.anly.githubapp.ui.module.main;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.anly.githubapp.GithubApplication;
 import com.anly.githubapp.R;
@@ -13,6 +17,7 @@ import com.anly.githubapp.di.component.MainComponent;
 import com.anly.githubapp.di.module.ActivityModule;
 import com.anly.githubapp.di.module.RepoModule;
 import com.anly.githubapp.ui.base.BaseActivity;
+import com.anly.githubapp.ui.module.account.LoginActivity;
 import com.anly.githubapp.ui.module.main.adapter.MainFragmentPagerAdapter;
 import com.gigamole.navigationtabbar.ntb.NavigationTabBar;
 
@@ -20,15 +25,19 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity implements HasComponent<MainComponent>{
+public class MainActivity extends BaseActivity implements HasComponent<MainComponent> {
 
-    private MainFragmentPagerAdapter mAdapter;
+    @BindView(R.id.main_toolbar)
+    Toolbar mToolbar;
 
     @BindView(R.id.view_pager)
     ViewPager mViewPager;
     @BindView(R.id.nav_bar)
     NavigationTabBar mNavBar;
+
+    private MainFragmentPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,8 @@ public class MainActivity extends BaseActivity implements HasComponent<MainCompo
     }
 
     private void initViews() {
+        setSupportActionBar(mToolbar);
+
         mAdapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mAdapter);
 
@@ -64,5 +75,30 @@ public class MainActivity extends BaseActivity implements HasComponent<MainCompo
                 .activityModule(new ActivityModule(this))
                 .repoModule(new RepoModule())
                 .build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_account:
+                LoginActivity.launch(this);
+                return true;
+
+            case R.id.action_settings:
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
