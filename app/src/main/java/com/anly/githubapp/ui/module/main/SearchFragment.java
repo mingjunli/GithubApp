d696e34da07ec7b6e583c8619205f32803f1f214
mingjunli <anly.pear@gmail.com>
2016-07-21 19:37:08 +0800

    add language selected listener

--- app/src/main/java/com/anly/githubapp/ui/module/main/SearchFragment.java
+++ app/src/main/java/com/anly/githubapp/ui/module/main/SearchFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -18,6 +19,7 @@ import com.anly.githubapp.di.component.MainComponent;
 import com.anly.githubapp.presenter.main.SearchPresenter;
 import com.anly.githubapp.ui.base.LceFragment;
 import com.anly.githubapp.ui.module.main.adapter.RepoListRecyclerAdapter;
+import com.jakewharton.rxbinding.widget.RxAdapterView;
 
 import java.util.ArrayList;
 
@@ -25,6 +27,7 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.functions.Action1;
 
 /**
  * Created by mingjun on 16/7/19.
@@ -69,10 +72,17 @@ public class SearchFragment extends LceFragment<ArrayList<Repo>> {
     private void initViews() {
         mPresenter.bindSearchView(mSearchKeyText);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
                 R.array.languages, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mLanguageSpinner.setAdapter(adapter);
+        RxAdapterView.itemSelections(mLanguageSpinner)
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer position) {
+                        mPresenter.setLanguage((String) adapter.getItem(position));
+                    }
+                });
 
         mAdapter = new RepoListRecyclerAdapter(null);
         mRepoListView.setLayoutManager(new LinearLayoutManager(getContext()));
