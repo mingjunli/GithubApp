a70f9fcbe5aa27a23058d1ed21992388246b22e7
mingjunli <anly.pear@gmail.com>
2016-09-02 10:12:05 +0800

    [Remove Ali baichuan] Desc: Remove ali baichuan's feedback & push.

--- app/src/main/java/com/anly/githubapp/common/wrapper/FeedbackPlatform.java
+++ app/src/main/java/com/anly/githubapp/common/wrapper/FeedbackPlatform.java
@@ -3,8 +3,6 @@ package com.anly.githubapp.common.wrapper;
 import android.app.Application;
 import android.content.Context;
 
-import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
-
 /**
  * Created by mingjun on 16/8/18.
  */
@@ -12,10 +10,9 @@ public class FeedbackPlatform {
 
     public static void init(Application application) {
         AppLog.d("FeedbackPlatform init");
-        FeedbackAPI.initAnnoy(application, "23436013");
     }
 
     public static void openFeedback(Context context) {
-        FeedbackAPI.openFeedbackActivity(context);
+        // TODO
     }
 }
--- app/src/main/java/com/anly/githubapp/common/wrapper/PushPlatform.java
+++ app/src/main/java/com/anly/githubapp/common/wrapper/PushPlatform.java
@@ -1,14 +1,6 @@
 package com.anly.githubapp.common.wrapper;
 
 import android.content.Context;
-import android.util.Log;
-
-import com.alibaba.sdk.android.AlibabaSDK;
-import com.alibaba.sdk.android.Environment;
-import com.alibaba.sdk.android.callback.InitResultCallback;
-import com.alibaba.sdk.android.push.CloudPushService;
-import com.alibaba.sdk.android.push.CommonCallback;
-import com.anly.githubapp.BuildConfig;
 
 /**
  * Created by mingjun on 16/8/17.
@@ -17,41 +9,7 @@ public class PushPlatform {
 
     public static void init(final Context context) {
         AppLog.d("PushPlatform init");
-
-        if (BuildConfig.DEBUG) {
-            AlibabaSDK.turnOnDebug();
-        }
-
-        AlibabaSDK.asyncInit(context.getApplicationContext(), new InitResultCallback() {
-            @Override
-            public void onSuccess() {
-                AppLog.d("PushSDK, init onSuccess.");
-                initCloudChannel(context);
-            }
-
-            @Override
-            public void onFailure(int i, String s) {
-                AppLog.w("PushSDK, init onFailure:" + s);
-            }
-        });
+        // TODO
     }
 
-    public static void initCloudChannel(Context context) {
-        CloudPushService pushService = AlibabaSDK.getService(CloudPushService.class);
-
-        AppLog.d("device id:" + pushService.getDeviceId());
-
-        pushService.register(context.getApplicationContext(), new CommonCallback() {
-            @Override
-            public void onSuccess() {
-                AppLog.d("PushSDK, initCloudChannel onSuccess.");
-
-            }
-
-            @Override
-            public void onFailed(String errorCode, String errorMessage) {
-                AppLog.w("PushSDK, initCloudChannel onFailure:" + errorMessage);
-            }
-        });
-    }
 }
